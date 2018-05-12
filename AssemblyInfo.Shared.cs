4d9e89778b2e121314e5532cd45837a810b1723c
lastunicorn <coldblackstar@yahoo.com>
2018-05-12 08:31:03 +0300

    added a common assembly info file.

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -15,23 +15,25 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DirectoryCompare")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Dust in the Wind")]
+[assembly: AssemblyProduct("Directory Compare")]
+[assembly: AssemblyCopyright("Copyright © Dust in the Wind 2018")]
+[assembly: AssemblyTrademark("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a2bf82b-a566-4d6b-9ada-3414dd8c12d8")]
-
-[assembly: InternalsVisibleTo("DirectoryCompare.Tests")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
--- DirectoryCompare/Properties/AssemblyInfo.cs
+++ DirectoryCompare/Properties/AssemblyInfo.cs
@@ -23,10 +23,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DirectoryCompare")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DirectoryCompare")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -36,16 +32,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("27eaa312-ec92-41ea-b854-00b5c9f2fc7b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
