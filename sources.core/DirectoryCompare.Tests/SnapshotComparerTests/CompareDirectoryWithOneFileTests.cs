76de37feb2840ea45609df65005ab880fcde3115
lastunicorn <coldblackstar@yahoo.com>
2019-12-22 09:30:18 +0200

    added the tests project.

---
+++ sources.core/DirectoryCompare.Tests/SnapshotComparerTests/CompareDirectoryWithOneFileTests.cs
@@ -0,0 +1,168 @@
+﻿// DirectoryCompare
+// Copyright (C) 2017-2019 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using DustInTheWind.DirectoryCompare.Comparison;
+using DustInTheWind.DirectoryCompare.Entities;
+using NUnit.Framework;
+
+namespace DustInTheWind.DirectoryCompare.Tests.SnapshotComparerTests
+{
+    [TestFixture]
+    public class CompareDirectoryWithOneFileTests
+    {
+        #region OnlyInSnapshot1
+
+        [Test]
+        public void OnlyInSnapshot1_is_empty_if_both_snapshotss_contain_one_identical_file_in_same_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            hDirectory1.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            hDirectory2.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot1, Is.EqualTo(new string[0]));
+        }
+
+        [Test]
+        public void OnlyInSnapshot1_contains_the_name_of_the_file_if_only_snapshot1_has_one_file_in_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            hDirectory1.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot1, Is.EqualTo(new[] { "/Dir1/File1" }));
+        }
+
+        [Test]
+        public void OnlyInSnapshot1_is_empty_if_only_snapshot2_has_one_file_in_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            hDirectory2.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot1, Is.EqualTo(new string[0]));
+        }
+
+        #endregion
+
+        #region OnlyInSnapshot2
+
+        [Test]
+        public void OnlyInSnapshot2_is_empty_if_both_snapshots_contain_one_identical_file_in_same_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            hDirectory1.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            hDirectory2.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot2, Is.EqualTo(new string[0]));
+        }
+
+        [Test]
+        public void OnlyInSnapshot2_contains_the_name_of_the_file_if_only_snapshot2_has_one_file_in_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            hDirectory2.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot2, Is.EqualTo(new[] { "/Dir1/File1" }));
+        }
+
+        [Test]
+        public void OnlyInSnapshot2_is_empty_if_only_snapshot1_has_one_file_in_dir()
+        {
+            Snapshot snapshot1 = new Snapshot();
+            HDirectory hDirectory1 = new HDirectory("Dir1");
+            hDirectory1.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1", Hash = new byte[] { 0x01, 0x02, 0x03 } }
+            });
+            snapshot1.Directories.AddRange(new[] { hDirectory1 });
+
+            Snapshot snapshot2 = new Snapshot();
+            HDirectory hDirectory2 = new HDirectory("Dir1");
+            snapshot2.Directories.AddRange(new[] { hDirectory2 });
+
+            SnapshotComparer comparer = new SnapshotComparer(snapshot1, snapshot2);
+            comparer.Compare();
+
+            Assert.That(comparer.OnlyInSnapshot2, Is.EqualTo(new string[0]));
+        }
+
+        #endregion
+    }
+}
---
+++ sources.core/DirectoryCompare.Tests/SnapshotComparerTests/IdenticalContainersWithSameFileTwiceTests.cs
@@ -0,0 +1,94 @@
+﻿// DirectoryCompare
+// Copyright (C) 2017-2019 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using DustInTheWind.DirectoryCompare.Comparison;
+using DustInTheWind.DirectoryCompare.Entities;
+using NUnit.Framework;
+
+namespace DustInTheWind.DirectoryCompare.Tests.SnapshotComparerTests
+{
+    [TestFixture]
+    public class IdenticalSnapshotsWithSameFileTwiceTests
+    {
+        private Snapshot snapshot1;
+        private Snapshot snapshot2;
+        private SnapshotComparer snapshotComparer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            snapshot1 = new Snapshot();
+            snapshot1.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1.txt", Hash = new byte[] { 1, 2, 3 } },
+                new HFile { Name = "File2.txt", Hash = new byte[] { 1, 2, 3 } }
+            });
+            snapshot2 = new Snapshot();
+            snapshot2.Files.AddRange(new[]
+            {
+                new HFile { Name = "File1.txt", Hash = new byte[] { 1, 2, 3 } },
+                new HFile { Name = "File2.txt", Hash = new byte[] { 1, 2, 3 } }
+            });
+            snapshotComparer = new SnapshotComparer(snapshot1, snapshot2);
+        }
+
+        [Test]
+        public void OnlyInSnapshot1_is_empty()
+        {
+            snapshotComparer.Compare();
+
+            Assert.That(snapshotComparer.OnlyInSnapshot1, Is.Empty);
+        }
+
+        [Test]
+        public void OnlyInSnapshot2_is_empty()
+        {
+            snapshotComparer.Compare();
+
+            Assert.That(snapshotComparer.OnlyInSnapshot2, Is.Empty);
+        }
+
+        [Test]
+        public void DifferentContent_is_empty()
+        {
+            snapshotComparer.Compare();
+
+            Assert.That(snapshotComparer.DifferentContent, Is.Empty);
+        }
+
+        [Test]
+        public void DifferentNames_contains_data()
+        {
+            snapshotComparer.Compare();
+
+            Assert.That(snapshotComparer.DifferentNames, Is.EqualTo(new[]
+            {
+                new ItemComparison
+                {
+                    RootPath = "/",
+                    Item1 = new HFile { Name = "File1.txt", Hash = new byte[] { 1, 2, 3 } },
+                    Item2 = new HFile { Name = "File2.txt", Hash = new byte[] { 1, 2, 3 } }
+                },
+                new ItemComparison
+                {
+                    RootPath = "/",
+                    Item1 = new HFile { Name = "File2.txt", Hash = new byte[] { 1, 2, 3 } },
+                    Item2 = new HFile { Name = "File1.txt", Hash = new byte[] { 1, 2, 3 } }
+                }
+            }));
+        }
+    }
+}
