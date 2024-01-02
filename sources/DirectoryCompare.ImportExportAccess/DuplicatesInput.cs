e02fe4e7a422a856688df33bb6acb742702980f7
Alexandru Iuga <alex.iuga@gmail.com>
2024-01-02 02:17:47 +0200

    clean up unused code.

--- sources/DirectoryCompare.ImportExportAccess/DuplicatesInput.cs
+++ sources/DirectoryCompare.ImportExportAccess/DuplicatesInput.cs
@@ -25,9 +25,6 @@ public class DuplicatesInput : IDuplicatesInput
     private readonly string path;
 
     private DuplicatesDocument duplicatesDocument;
-    // private JsonTextReader jsonTextWriter;
-    // private DuplicateFileState state;
-    // private DuplicatesHeader duplicatesHeader;
 
     public DuplicatesInput(string path)
     {
@@ -42,11 +39,6 @@ public class DuplicatesInput : IDuplicatesInput
         using JsonTextReader jsonTextReader = new(streamReader);
 
         duplicatesDocument = serializer.Deserialize<DuplicatesDocument>(jsonTextReader);
-
-        // StreamReader streamReader = new(path);
-        // jsonTextWriter = new JsonTextReader(streamReader);
-        //
-        // MoveToOpenedState();
     }
 
     public DuplicatesHeader GetHeader()
@@ -56,52 +48,7 @@ public class DuplicatesInput : IDuplicatesInput
             PotNameLeft = duplicatesDocument.Left,
             PotNameRight = duplicatesDocument.Right
         };
-
-        //return duplicatesHeader ??= ReadHeader();
     }
-
-    // public DuplicatesHeader ReadHeader()
-    // {
-    //     MoveToHeaderState();
-    //
-    //     duplicatesHeader = new DuplicatesHeader();
-    //
-    //     while (jsonTextWriter.Read())
-    //     {
-    //         if (jsonTextWriter.TokenType == JsonToken.Comment)
-    //             continue;
-    //
-    //         if (jsonTextWriter.TokenType == JsonToken.PropertyName)
-    //         {
-    //             bool isLeftProperty = string.Equals(jsonTextWriter.Value as string, "Left", StringComparison.InvariantCultureIgnoreCase);
-    //             if (isLeftProperty)
-    //             {
-    //                 duplicatesHeader.PotNameLeft = jsonTextWriter.ReadAsString();
-    //                 continue;
-    //             }
-    //
-    //             bool isRightProperty = string.Equals(jsonTextWriter.Value as string, "Right", StringComparison.InvariantCultureIgnoreCase);
-    //             if (isRightProperty)
-    //             {
-    //                 duplicatesHeader.PotNameRight = jsonTextWriter.ReadAsString();
-    //                 continue;
-    //             }
-    //
-    //             bool isDuplicatesProperty = string.Equals(jsonTextWriter.Value as string, "Duplicates", StringComparison.InvariantCultureIgnoreCase);
-    //             if (isDuplicatesProperty)
-    //             {
-    //                 state = DuplicateFileState.Duplicates;
-    //                 break;
-    //             }
-    //
-    //             throw new Exception("Invalid property detected: " + jsonTextWriter.Value);
-    //         }
-    //
-    //         throw new Exception("Could not read the duplicates json file. Invalid structure.");
-    //     }
-    //
-    //     return duplicatesHeader;
-    // }
 
     public IEnumerable<FileDuplicateGroup> EnumerateDuplicates()
     {
@@ -112,104 +59,5 @@ public class DuplicatesInput : IDuplicatesInput
                 FileSize = x.FileSize,
                 FileHash = FileHash.Parse(x.FileHash)
             });
-
-        // MoveToDuplicatesState();
-        //
-        // while (jsonTextWriter.Read())
-        // {
-        //     if (jsonTextWriter.TokenType == JsonToken.Comment)
-        //         continue;
-        //
-        //     if (jsonTextWriter.TokenType == JsonToken.StartArray)
-        //         break;
-        //
-        //     throw new Exception("Invalid file structure.");
-        // }
-        //
-        // yield return ReadFileDuplicateGroup();
     }
-
-    // private FileDuplicateGroup ReadFileDuplicateGroup()
-    // {
-    //     while (jsonTextWriter.Read())
-    //     {
-    //         if (jsonTextWriter.TokenType == JsonToken.Comment)
-    //             continue;
-    //
-    //         if (jsonTextWriter.TokenType == JsonToken.StartObject)
-    //             break;
-    //
-    //         throw new Exception("Invalid file structure.");
-    //     }
-    //     
-    //     
-    // }
-    //
-    // private void Read
-    //
-    // private void MoveToOpenedState()
-    // {
-    //     switch (state)
-    //     {
-    //         case DuplicateFileState.None:
-    //             while (jsonTextWriter.Read())
-    //             {
-    //                 if (jsonTextWriter.TokenType != JsonToken.Comment)
-    //                     break;
-    //             }
-    //
-    //             if (jsonTextWriter.TokenType != JsonToken.StartObject)
-    //                 throw new Exception("Invalid input file. The file should contain an object as a root token.");
-    //
-    //             state = DuplicateFileState.Opened;
-    //             break;
-    //
-    //         case DuplicateFileState.Opened:
-    //             break;
-    //
-    //         case DuplicateFileState.Header:
-    //         case DuplicateFileState.Duplicates:
-    //         case DuplicateFileState.Closed:
-    //         default:
-    //             throw new InvalidFileStateException(state.ToString());
-    //     }
-    // }
-    //
-    // private void MoveToHeaderState()
-    // {
-    //     switch (state)
-    //     {
-    //         case DuplicateFileState.Opened:
-    //             state = DuplicateFileState.Header;
-    //             break;
-    //
-    //         case DuplicateFileState.Header:
-    //             break;
-    //
-    //         case DuplicateFileState.None:
-    //         case DuplicateFileState.Duplicates:
-    //         case DuplicateFileState.Closed:
-    //         default:
-    //             throw new InvalidFileStateException(state.ToString());
-    //     }
-    // }
-    //
-    // private void MoveToDuplicatesState()
-    // {
-    //     switch (state)
-    //     {
-    //         case DuplicateFileState.Opened:
-    //         case DuplicateFileState.Header:
-    //             _ = ReadHeader();
-    //             break;
-    //
-    //         case DuplicateFileState.Duplicates:
-    //             break;
-    //
-    //         case DuplicateFileState.None:
-    //         case DuplicateFileState.Closed:
-    //         default:
-    //             throw new InvalidFileStateException(state.ToString());
-    //     }
-    // }
 }
