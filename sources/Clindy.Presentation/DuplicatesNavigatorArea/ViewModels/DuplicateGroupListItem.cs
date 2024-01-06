9ec5d6d36d8cdc186a71125465ce16e055deb877
Alexandru Iuga <alex.iuga@gmail.com>
2024-01-06 23:39:34 +0200

    update copyright year

--- sources/Clindy.Presentation/DuplicatesNavigatorArea/ViewModels/DuplicateGroupListItem.cs
+++ sources/Clindy.Presentation/DuplicatesNavigatorArea/ViewModels/DuplicateGroupListItem.cs
@@ -1,5 +1,5 @@
 // DirectoryCompare
-// Copyright (C) 2017-2023 Dust in the Wind
+// Copyright (C) 2017-2024 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
